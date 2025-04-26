090696c15de68c7b17a7380b295c50e07415b2d8
Samhitha2387866 <Velevela.Samhitha@cognizant.com>
2025-04-26 12:34:34 +0530

    IAppointmentRepository.cs

--- HospitalAppointments/Repository/IAppointmentRepository.cs
+++ HospitalAppointments/Repository/IAppointmentRepository.cs
@@ -1,20 +1,19 @@
-﻿using NewHospitalManagementSystem.Models;
-
+using NewHospitalManagementSystem.Models;
 using System.Collections.Generic;
-
 using System.Threading.Tasks;
+using System;
 
 public interface IAppointmentRepository
-
 {
-
     Task<Appointment> BookAppointmentAsync(Appointment appointment);
-
     Task<Appointment> GetAppointmentByIdAsync(int appointmentId);
-
     Task<IEnumerable<Appointment>> GetAllAppointmentsAsync();
+    Task<IEnumerable<Appointment>> GetAppointmentsByDoctorIdAsync(int doctorId);
+    Task<IEnumerable<Appointment>> GetAppointmentsByPatientIdAsync(int patientId);
+    Task<bool> CancelAppointmentAsync(int appointmentId);
+    Task<bool> PatientExistsAsync(int patientId);
+    Task<bool> CancelAppointmentForPatientAsync(int appointmentId, int patientId);
 
-    Task<bool> UpdateAppointmentStatusAsync(int appointmentId, string status);
-
+    Task<bool> IsDoctorAvailableAsync(int doctorId, DateOnly date, TimeSpan time);
+    Task<bool> HasOverlappingAppointment(int doctorId, DateOnly date, TimeSpan startTime, TimeSpan endTime);
 }
-
